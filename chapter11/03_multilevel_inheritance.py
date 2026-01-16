68bc1d67777b54de79e01b32ebcf90b693991efd
Knight69 <lakshitchoudary6@gmail.com>
2026-01-16 15:14:41 +0530

    added

---
+++ chapter11/03_multilevel_inheritance.py
@@ -0,0 +1,18 @@
+class Employee:
+    a = 1
+
+class Programmer(Employee):
+    b = 2
+
+class Manager(Programmer):
+    c = 3
+
+O = Employee
+print(O.a) # prints the a atttribute 
+# print(O.b) # shows an error as the is no b atttribute in a Employee class
+
+O =Programmer
+print(O.a,O.b)
+
+O =Manager
+print(O.a,O.b,O.c)
