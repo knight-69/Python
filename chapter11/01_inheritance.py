ec4c9055f82723d249a1ba4901f315af966b3eb5
Knight69 <lakshitchoudary6@gmail.com>
2026-01-16 14:50:21 +0530

    added

--- chapter11/01_inheritance.py
+++ chapter11/01_inheritance.py
@@ -1,2 +1,22 @@
-pyfkfefeqweqeq
-asdnknd
+class Employee:
+    company ="ITC"
+    def show(self):
+        print(f"the name is {self.mame} and the salary is {self.salary}")
+
+# class Programmer:
+#     company ="ITC Infotech"
+#     def show(self):
+#         print(f"the name is {self.name} and the salary is {self.salary}")
+
+#     def ShowLanguage(self):
+#         print(f"The name is {self.name} and he is a good with {self.language} language")
+
+class Programmer(Employee):
+    company ="ITC Infotech"
+    def ShowLanguage(self):
+        print(f"The name is {self.name} and he is a good with {self.language} language")
+    
+a =Employee
+b=Programmer
+
+print(a.company,b.company)
